3ba9753cefdd4bb65d9deea81c4ec2955344d91f
minorsevenflatfive <bramanta.n@gmail.com>
2023-12-19 10:35:15 +0700

    add a number of contentview

---
+++ Today/ContentViews/DatePickerContentView.swift
@@ -0,0 +1,50 @@
+//
+//  DatePickerContentView.swift
+//  Today
+//
+//  Created by Bramanta Nararya Nurul Haq on 19/12/23.
+//
+
+import Foundation
+import UIKit
+
+class DatePickerContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var date = Date.now
+
+        func makeContentView() -> UIView & UIContentView {
+            return DatePickerContentView(self)
+        }
+    }
+
+    let datePicker = UIDatePicker()
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        addPinnedSubview(datePicker)
+        datePicker.preferredDatePickerStyle = .inline
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        datePicker.date = configuration.date
+    }
+}
+
+
+
+extension UICollectionViewListCell {
+    func datePickerConfiguration() -> DatePickerContentView.Configuration {
+        DatePickerContentView.Configuration()
+    }
+}
---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Bramanta Nararya Nurul Haq on 14/12/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
